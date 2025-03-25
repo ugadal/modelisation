45ee8d91e0a2181f296e15dba97131f25f0fbac1
Ugadal <dcoorna@gmail.com>
2025-03-25 17:20:59 +0100

    up

--- mymal.py
+++ mymal.py
@@ -5,22 +5,25 @@
 from hmm4malib  import *
 random.seed(.31415)
 LS=[Seq("ABC"),Seq("BCD")]
-print(LS)
+# ~ print(LS)
 model=mkrndmodel(LS)
-print (",".join(list(model.PI.keys())))
-print (",".join(map(str,list(model.PI.values()))))
-for s in (I,M):
-	print("Em,"+",".join(model.LM[0].M.E.keys()))
+print (";".join(list(model.PI.keys())))#Pi R1
+print (";".join(map(str,list(model.PI.values()))))
+# ~ for s in (I,M,D):
+	# ~ print(";".join())
+for s in (I,M): #Em ix row 4+0.1.2.3... , mx 12=4+N+2 
+	print("Em;"+";".join(model.LM[0].M.E.keys()))
 	for mod in model.LM:
-		print(f"{s}{mod.col},"+",".join(map(str,list(mod.st(s).E.values()))))
-for s in (M,D):
-	print("tr,"+",".join(model.LM[0].M.T.keys()))
+		print(f"{s}{mod.col},"+";".join(map(str,list(mod.st(s).E.values()))))
+for s in (M,D): #tr Mx row 20=4+2*N+2+2, Dx = 27
+	print("tr;"+";".join(model.LM[0].M.T.keys()))
 	for mod in model.LM[:-1]:
-		print(f"{s}{mod.col},"+",".join(map(str,list(mod.st(s).T.values()))))
+		print(f"{s}{mod.col},"+";".join(map(str,list(mod.st(s).T.values()))))
 for s in (I):
-	print("tr,"+",".join(model.LM[0].M.T.keys()))
+	print("tr;"+",".join(model.LM[0].M.T.keys()))
 	for mod in model.LM:
-		print(f"{s}{mod.col},"+",".join(map(str,list(mod.st(s).T.values()))))
+		print(f"{s}{mod.col},"+";".join(map(str,list(mod.st(s).T.values()))))
+exit()
 fs=LS[0]
 print(fs)
 fs.alphab(model)
